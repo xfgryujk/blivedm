0091ed9a778b1d4bf6b9f8374ff173c4dc3ecc6b
John Smith <xfgryujk@126.com>
2019-02-19 23:15:00 +0800

    Revert "几乎完全重写"
    This reverts commit 07bc699f2cf5a259374daace48940c86be825349.

--- sample.py
+++ sample.py
@@ -1,24 +1,48 @@
 # -*- coding: utf-8 -*-
 
-import asyncio
-from time import time
-from blivedm import DanmuPrinter
+import sys
+from asyncio import get_event_loop
+from ssl import SSLError
 
-async def test1():
-    connection = DanmuPrinter(23058, 0)
-    task_run = asyncio.ensure_future(connection.run_forever())
-    await asyncio.sleep(30)
-    print(time(), 'closing')
-    await connection.close()
-    print(time(), 'closed')
-    await task_run
-    print(time(), 'all done')
+from blivedm import BLiveClient
+
+
+class MyBLiveClient(BLiveClient):
+
+    async def _on_get_popularity(self, popularity):
+        print('当前人气值：', popularity)
+
+    async def _on_get_danmaku(self, content, user_name):
+        print(user_name, '说：', content)
+
+    def _on_stop(self, exc):
+        self._loop.stop()
+
+    def _handle_error(self, exc):
+        print(exc, file=sys.stderr)
+        if isinstance(exc, SSLError):
+            print('SSL验证失败！', file=sys.stderr)
+        return False
 
 
 def main():
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(test1())
-    loop.close()
+    loop = get_event_loop()
+
+    # 如果SSL验证失败就把第二个参数设为False
+    client = MyBLiveClient(139, True)
+    client.start()
+
+    # 5秒后停止，测试用
+    # loop.call_later(5, client.stop)
+    # 按Ctrl+C停止
+    # import signal
+    # signal.signal(signal.SIGINT, lambda signum, frame: client.stop())
+
+    try:
+        loop.run_forever()
+    finally:
+        loop.run_until_complete(loop.shutdown_asyncgens())
+        loop.close()
 
 
 if __name__ == '__main__':
