3bfb0370aca941942e42bfe365faf77c81f966bd
HongLouWang <shutian.wang@wsu.edu>
2026-03-15 04:16:19 +0800

    add user-agent setter and user-agent randomizer to bypass -352 error in init_room

--- blivedm/utils.py
+++ blivedm/utils.py
@@ -1,7 +1,38 @@
+from random_user_agent.user_agent import UserAgent
+from random_user_agent.params import SoftwareName, OperatingSystem
+
 # -*- coding: utf-8 -*-
 USER_AGENT = (
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'
 )
+
+def randomlize_user_agent():
+    """
+    随机生成一个User-Agent字符串，并赋值给全局USER_AGENT变量。
+    返回新的User-Agent。
+    """
+
+    software_names = [SoftwareName.CHROME.value]
+    operating_systems = [OperatingSystem.WINDOWS.value]
+
+    user_agent_rotator = UserAgent(
+        software_names=software_names,
+        operating_systems=operating_systems
+    )
+
+    global USER_AGENT
+    USER_AGENT = user_agent_rotator.get_random_user_agent()
+    return USER_AGENT
+
+def customize_user_agent(ua: str):
+    """
+    设置全局USER_AGENT变量为指定的ua字符串。
+    返回新的User-Agent。
+    """
+
+    global USER_AGENT
+    USER_AGENT = ua
+    return USER_AGENT
 
 
 def make_constant_retry_policy(interval: float):
