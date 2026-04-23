a5d38ab2d4a4f0304654a4970dd33f167ddc0898
John Smith <xfgryujk@126.com>
2026-04-23 20:11:57 +0800

    更新UA
    fix #83, #84

--- blivedm/clients/ws_base.py
+++ blivedm/clients/ws_base.py
@@ -14,10 +14,6 @@ from .. import handlers, utils
 
 logger = logging.getLogger('blivedm')
 
-USER_AGENT = (
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'
-)
-
 HEADER_STRUCT = struct.Struct('>I2H2I')
 
 
--- blivedm/utils.py
+++ blivedm/utils.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 USER_AGENT = (
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/147.0.0.0 Safari/537.36'
 )
 
 
